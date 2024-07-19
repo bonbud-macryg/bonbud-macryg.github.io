3f9e97fbeaff3d0caa35b08aa50508c0c5f54427
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-07-19 18:44:24 +0100

    cabbit

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', function(e) {
   var options = {
     body: 'This notification was generated from a push!',
-    icon: 'images/example.png',
+    icon: 'https://ewr1.vultrobjects.com/urbit/hastuc-dibtux/2024.4.30..2.37.9..bae1.47ae.147a.e147-IMG_5592.jpeg',
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
@@ -16,7 +16,7 @@ self.addEventListener('push', function(e) {
       {
         action: 'close',
         title: 'Close',
-        icon: 'images/xmark.png'
+        icon: 'https://ewr1.vultrobjects.com/urbit/hastuc-dibtux/2024.4.30..2.37.9..bae1.47ae.147a.e147-IMG_5592.jpeg'
       }
     ]
   };
