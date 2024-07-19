5fbfa242ff77879d8ab9027addaaa2e8b4f8709c
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-07-19 18:40:49 +0100

    new endpoint

--- push.js
+++ push.js
@@ -8,5 +8,5 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/clq2lq83lzs:APA91bGXeTxGDYLve7hQNmSfkcSI4dGuUj6AKRZC4A_rDf6b7GVuIzw_WBaWI_Ab9qghVtABtuj3QnWVMhqyrjNdvi9lV4BxBOY7LYNre7AuPtn9mQyNyqzTT2qEvF0fZE9A4TAY71lQ","expirationTime":null,"keys":{"p256dh":"BHPyOZDxazqITnf_lCgWqB2hcD8FWcaRECmpWjNhDbB7x_00SWlYdUjA5cFa7HD52UwQcWjM5PiA9wbQa9u3YDc","auth":"AKYoZrF5IdOzvJObHtEZRg"}}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c5xlxP_aluk:APA91bGol3CXRzIf8HfT7NviYJ5x-9t_fpc35_aHBbj47ygwSV0y0EQd_kaEZevn4jnbKei_bnSdpFAihDdnLxVwUkVnSho6u6f_5g_t8x3xNP9g9S0fVulfKV4DGK6v2sa-Q9It-kJY","expirationTime":null,"keys":{"p256dh":"BFYHTj6ntcc_6QUK8ZArY9dpVM6yOI0EETSk5EwzS5sYR-uXk2KXQoBCWRTga-VpRUDPIOKpIsLWhObjxg5w2Q0","auth":"au11zPbH_icBxY1t3yV7Nw"}}
 push.sendNotification(sub, 'test message')
